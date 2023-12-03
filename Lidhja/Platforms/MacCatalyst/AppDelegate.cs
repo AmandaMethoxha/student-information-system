a190fcf7088e36f85432ea31594f541950fea1bb
Amanda Methoxha <amandamethoxha@Amandas-MacBook-Pro.local>
2023-12-03 20:09:13 +0000

    first commit

---
+++ Lidhja/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Lidhja;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
